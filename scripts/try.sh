2e5e61e353f6609128b71d89fb6e9dca28825c20
“TmeLoverccâ <gzjz07@outlook.com>
2023-11-22 03:01:06 +0000

    add spnet

--- scripts/try.sh
+++ scripts/try.sh
@@ -1,3 +1,3 @@
 
 # try bail GCN
-CUDA_VISIBLE_DEVICES=0 python ./src/main.py --dataset_name spmotif --backbone_name pna --model_name erm --seed 3
+CUDA_VISIBLE_DEVICES=0 python ./src/main.py --dataset_name spmotif --backbone_name spnet --model_name erm --seed 3
---
+++ src/backbones/spnet.py
@@ -0,0 +1,88 @@
+# From Discovering Invariant Rationales for Graph Neural Networks
+
+import torch
+import torch.nn as nn
+from torch.nn import Linear, ReLU, ModuleList
+from torch_geometric.nn import global_mean_pool
+
+from conv_layers import LEConv
+
+
+class SPNET(nn.Module):
+    def __init__(self, backbone_config):
+        super().__init__()
+        self.gc_layer = gc_layer = backbone_config['gc_layer']
+        self.in_dim = in_dim = backbone_config['in_dim']
+        self.edge_attr_dim = edge_attr_dim = backbone_config['edge_attr_dim'] 
+        self.hidden_dim = hidden_dim = backbone_config['hidden_dim']
+        self.out_dim = out_dim = backbone_config['out_dim']
+        self.p = dropout = backbone_config['dropout']
+        self.use_edge_attr = backbone_config.get('use_edge_attr', True)
+
+        self.node_encoder = Linear(in_dim, hidden_dim)
+
+        self.convs = ModuleList()
+        self.relus = ModuleList()
+        for i in range(self.gc_layer):
+            conv = LEConv(in_channels=hidden_dim, out_channels=hidden_dim)
+            self.convs.append(conv)
+            self.relus.append(ReLU())
+
+        self.pool = global_mean_pool
+
+        self.fc = nn.Sequential(
+            Linear(hidden_dim, 2*hidden_dim),
+            ReLU(),
+            Linear(2*hidden_dim, out_dim)
+        )
+        self.conf_mlp = nn.Sequential(
+            Linear(hidden_dim, 2*hidden_dim),
+            ReLU(),
+            Linear(2*hidden_dim, 3)
+        )
+        self.cq = Linear(3, 3)
+        self.conf_fw = nn.Sequential(
+            self.conf_mlp,
+            self.cq
+        )
+
+    def forward(self, batch, edge_att=None):
+        batch_idx = batch['batch']
+        node_x = self.get_emb(batch, edge_att=edge_att)
+        graph_x = self.pool(node_x, batch_idx)
+        return self.get_causal_pred(graph_x)
+
+    def get_emb(self, batch, edge_att=None):
+        x, edge_index, edge_attr, batch = batch['x'], batch['edge_index'], batch['edge_attr'], batch['batch']
+        x = self.node_encoder(x)
+        for i, (conv, relu) in enumerate(zip(self.convs, self.relus)):
+            x = conv(x=x, edge_index=edge_index, edge_weight=edge_attr, edge_atten=edge_att)
+            x = relu(x)
+        return x
+
+    def get_pred_from_emb(self, emb, batch):
+        return self.fc(self.pool(emb, batch))
+
+    def get_graph_emb(self, batch, edge_att=None):
+        batch_idx = batch['batch']
+        node_x = self.get_emb(batch, edge_att=edge_att)
+        graph_x = self.pool(node_x, batch_idx)
+        return graph_x
+
+    def get_causal_pred(self, causal_graph_x):
+        pred = self.fc(causal_graph_x)
+        return pred
+
+    def get_conf_pred(self, conf_graph_x):
+        pred = self.conf_fw(conf_graph_x)
+        return pred
+
+    def get_comb_pred(self, causal_graph_x, conf_graph_x):
+        causal_pred = self.fc(causal_graph_x)
+        conf_pred = self.conf_mlp(conf_graph_x).detach()
+        return torch.sigmoid(conf_pred) * causal_pred
+
+    def reset_parameters(self):
+        with torch.no_grad():
+            for param in self.parameters():
+                param.uniform_(-1.0, 1.0)
