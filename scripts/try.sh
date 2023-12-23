0a158328ae9822323446419e4ac88811a487332a
TimeLovercc <gzjz07@outlook.com>
2023-12-22 20:47:52 -0500

    remove redundant

--- scripts/try.sh
+++ scripts/try.sh
@@ -1,3 +1,3 @@
 
 # try bail GCN
-CUDA_VISIBLE_DEVICES=6 python ./src/main.py --dataset_name spmotif --backbone_name cigagin --model_name dir --seed 3
+CUDA_VISIBLE_DEVICES=3 python ./src/main.py --dataset_name spmotif --backbone_name cigagin --model_name dir --seed 3
